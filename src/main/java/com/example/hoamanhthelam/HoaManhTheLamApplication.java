d6475199785c307ac3c9d88ffb545156bd575263
High Lam <hhighlam2004@gmail.com>
2024-09-10 21:30:34 +0700

    lam add

---
+++ src/main/java/com/example/hoamanhthelam/HoaManhTheLamApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoamanhthelam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaManhTheLamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoaManhTheLamApplication.class, args);
+    }
+
+}
